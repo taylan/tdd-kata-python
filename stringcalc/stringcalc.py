0226a4d0ba89ea7e6c54c1c58c4b5d77861e4d9c
Taylan Aydinli <taylanaydinli@gmail.com>
2014-03-19 15:37:44 +0200

    Default seperator is now comma. Added tests and implementation for some edge cases

--- stringcalc/stringcalc.py
+++ stringcalc/stringcalc.py
@@ -1,19 +1,30 @@
 
 
 class StringCalculator():
-    def _parse_single_number(self, inp):
+    SEPERATORS = ','
+
+    @staticmethod
+    def _parse_single_number(inp):
         try:
             return int(inp)
         except ValueError:
             return None
 
+    def _split_numbers(self, inp):
+        lines = [l for l in inp.splitlines()]
+        if '\n' in lines:
+            if [l for l in lines if l.strip() == '']:
+                return None
+        return ','.join(lines).strip().split(self.SEPERATORS)
+
     def _parse_multiple_numbers(self, inp):
         try:
-            return sum(map(int, inp.strip().split(' ')))
-        except ValueError:
+            return sum(map(int, self._split_numbers(inp)))
+        except (ValueError, TypeError):
             return None
 
     def add(self, inp):
+        inp = inp.strip()
         if not inp:
             return 0
 
--- stringcalc/tests.py
+++ stringcalc/tests.py
@@ -1,4 +1,4 @@
-from unittest import TestCase
+from unittest import TestCase, skip
 from stringcalc import StringCalculator
 
 
@@ -10,9 +10,18 @@ class StringCalculatorTest(TestCase):
     def test_empty_string_gives_0(self):
         self.assertEqual(self.target.add(''), 0)
 
+    def test_multiple_empty_string_gives_0(self):
+        self.assertEqual(self.target.add(' '), 0)
+
     def test_single_number_gives_itself(self):
         self.assertEqual(self.target.add('1'), 1)
         self.assertEqual(self.target.add('123'), 123)
 
     def test_two_numbers_gives_sum(self):
-        self.assertEqual(self.target.add('1 2'), 3)
+        self.assertEqual(self.target.add('1,2'), 3)
+
+    def test_new_line_allowed_as_seperator(self):
+        self.assertEqual(self.target.add('1\n2,3'), 6)
+
+    def test_new_line_with_empty_line_disallowed(self):
+        self.assertEqual(self.target.add('1,\n'), '')
