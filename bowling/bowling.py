0bc936344b4a76cf6ef0b28c4141d1436b31a165
Taylan Aydinli <taylanaydinli@gmail.com>
2014-03-25 13:57:42 +0200

    More tests. roll method knocks down random number of pins if no parameter is specified.

--- bowling/bowling.py
+++ bowling/bowling.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from random import randrange
 
 
 class GameStates(Enum):
@@ -9,6 +10,7 @@ class BowlingGame():
     def __init__(self):
         self._current_frame = 1
         self._game_state = GameStates.Pending
+        self._last_throw_count = None
 
     @property
     def current_frame(self):
@@ -18,9 +20,14 @@ class BowlingGame():
     def game_state(self):
         return self._game_state
 
-    def roll(self):
+    @property
+    def last_throw_count(self):
+        return self._last_throw_count
+
+    def roll(self, pins=None):
         if self._current_frame == 10:
             self._game_state = GameStates.Finished
         else:
+            self._last_throw_count = pins or randrange(0, 11)
             self._current_frame += 1
             self._game_state = GameStates.InProgress
--- bowling/tests.py
+++ bowling/tests.py
@@ -1,8 +1,13 @@
-from unittest import TestCase
 from bowling import BowlingGame, GameStates
+from unittest import TestCase
 
 
-class BowlingGameTest(TestCase):
+class BowlingGameTestCaseBase(TestCase):
+    def assertBetween(self, val, first, second, msg=None):
+        self.assertTrue(first <= val <= second, msg)
+
+
+class BowlingGameTest(BowlingGameTestCaseBase):
     def setUp(self):
         self.target = BowlingGame()
 
@@ -13,17 +18,32 @@ class BowlingGameTest(TestCase):
         self.assertEqual(self.target.game_state, GameStates.Pending)
 
     def test_first_roll_updates_game_state_to_in_progress(self):
-        self.target.roll()
+        self.target.roll(1)
         self.assertEqual(self.target.game_state, GameStates.InProgress)
 
     def test_one_roll_increments_frame_by_one(self):
-        self.target.roll()
+        self.target.roll(1)
         self.assertEqual(self.target.current_frame, 2)
 
     def test_game_finishes_after_10_frames(self):
         for i in range(10):
-            self.target.roll()
+            self.target.roll(1)
 
         self.assertEqual(self.target.current_frame, 10)
         self.assertEqual(self.target.game_state, GameStates.Finished)
 
+    def test_game_remembers_last_knocked_down_pin_count(self):
+        self.target.roll(1)
+        self.assertEqual(self.target.last_throw_count, 1)
+
+    def test_roll_without_specified_score_knocks_down_random_pins(self):
+        self.target.roll()
+        self.assertBetween(self.target.last_throw_count, 0, 10)
+
+    def test_knock_down_less_than_10_pins_frame_does_not_change(self):
+        self.target.roll(5)
+        self.assertBetween(self.target.current_frame, 1)
+
+    # def test_strike_increments_frame(self):
+    #     self.target.roll(10)
+    #     self.assertEqual(self.target.current_frame, 2)
