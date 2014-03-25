66438e2474b29039091cc6c09b226f2602798e3e
Taylan Aydinli <taylanaydinli@gmail.com>
2014-03-25 14:41:38 +0200

    Large refactoring. Frames now managed through BowlingFrame objects

--- bowling/bowling.py
+++ bowling/bowling.py
@@ -3,31 +3,63 @@ from random import randrange
 
 
 class GameStates(Enum):
-    Pending, InProgress, Finished = range(3)
+    InProgress, Finished = range(2)
+
+
+class BowlingFrame():
+    def __init__(self, num):
+        self._num = num
+        self._rolls = []
+
+    @property
+    def is_complete(self):
+        return len(self._rolls) == 2
+
+    @property
+    def last_roll(self):
+        return self._rolls[-1] if self._rolls else None
+
+    def do_roll(self, num):
+        self._rolls.append(num)
+
+    def __str__(self):
+        return '<BowlingFrame({0}), rolls: {1}>'.format(self._num, self._rolls)
+
+    def __repr__(self):
+        return str(self)
 
 
 class BowlingGame():
     def __init__(self):
-        self._current_frame = 1
-        self._game_state = GameStates.Pending
-        self._last_throw_count = None
+        self._frames = [BowlingFrame(1)]
 
     @property
-    def current_frame(self):
-        return self._current_frame
+    def current_frame_number(self):
+        return len(self._frames)
 
     @property
     def game_state(self):
-        return self._game_state
+        return GameStates.InProgress if len(self._frames) != 10\
+            or [frm for frm in self._frames if not frm.is_complete] \
+            else GameStates.Finished
 
     @property
     def last_throw_count(self):
-        return self._last_throw_count
+        return self._frames[-1].last_roll
 
     def roll(self, pins=None):
-        if self._current_frame == 10:
-            self._game_state = GameStates.Finished
+        if self.game_state == GameStates.Finished:
+            pass
+
+        throw = pins or randrange(0, 11)
+        if not self._frames[-1].is_complete:
+            self._frames[-1].do_roll(throw)
+            if len(self._frames) < 10 and self._frames[-1].is_complete:
+                self._frames.append(BowlingFrame(len(self._frames) + 1))
         else:
-            self._last_throw_count = pins or randrange(0, 11)
-            self._current_frame += 1
-            self._game_state = GameStates.InProgress
+            frm = BowlingFrame(len(self._frames) + 1)
+            frm.do_roll(throw)
+            self._frames.append(frm)
+
+    def __str__(self):
+        return '<BowlingGame frm: {0}>'.format(self.current_frame_number)
--- bowling/tests.py
+++ bowling/tests.py
@@ -1,5 +1,5 @@
 from bowling import BowlingGame, GameStates
-from unittest import TestCase
+from unittest import TestCase, skip
 
 
 class BowlingGameTestCaseBase(TestCase):
@@ -12,24 +12,16 @@ class BowlingGameTest(BowlingGameTestCaseBase):
         self.target = BowlingGame()
 
     def test_game_starts_at_frame_1(self):
-        self.assertEqual(self.target.current_frame, 1)
+        self.assertEqual(self.target.current_frame_number, 1)
 
-    def test_game_starts_at_pending_state(self):
-        self.assertEqual(self.target.game_state, GameStates.Pending)
-
-    def test_first_roll_updates_game_state_to_in_progress(self):
-        self.target.roll(1)
+    def test_game_starts_at_in_progress_state(self):
         self.assertEqual(self.target.game_state, GameStates.InProgress)
 
-    def test_one_roll_increments_frame_by_one(self):
-        self.target.roll(1)
-        self.assertEqual(self.target.current_frame, 2)
-
     def test_game_finishes_after_10_frames(self):
-        for i in range(10):
+        for i in range(20):
             self.target.roll(1)
 
-        self.assertEqual(self.target.current_frame, 10)
+        self.assertEqual(self.target.current_frame_number, 10)
         self.assertEqual(self.target.game_state, GameStates.Finished)
 
     def test_game_remembers_last_knocked_down_pin_count(self):
@@ -42,7 +34,12 @@ class BowlingGameTest(BowlingGameTestCaseBase):
 
     def test_knock_down_less_than_10_pins_frame_does_not_change(self):
         self.target.roll(5)
-        self.assertBetween(self.target.current_frame, 1)
+        self.assertBetween(self.target.current_frame_number, 0, 10)
+
+    def test_frame_advances_after_two_rolls(self):
+        self.target.roll(5)
+        self.target.roll(2)
+        self.assertEqual(self.target.current_frame_number, 2)
 
     # def test_strike_increments_frame(self):
     #     self.target.roll(10)
