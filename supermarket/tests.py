bdb01e759552c59e79115ef0a195cb092a5f6d8d
Taylan Aydinli <taylanaydinli@gmail.com>
2014-04-08 13:28:29 +0300

    Added more tests

--- supermarket/tests.py
+++ supermarket/tests.py
@@ -11,3 +11,13 @@ class SupermarketCheckoutTestCase(unittest.TestCase):
         self.target.scan(item1)
 
         self.assertEqual(self.target.total, 10)
+
+    def test_multiple_item_prices_calculated_correctly(self):
+        item1 = Item('i1', 10)
+        item2 = Item('i2', 30)
+
+        self.target.scan(item1)
+        self.target.scan(item2)
+
+        self.assertEqual(self.target.total, 40)
+
