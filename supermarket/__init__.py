ded11b58aa5cd0a7fe689e4da76b53478b74360e
Taylan Aydinli <taylanaydinli@gmail.com>
2014-04-10 20:57:45 +0300

    Refactored CheckoutRegister class to properly store actual product information

--- supermarket/__init__.py
+++ supermarket/__init__.py
@@ -14,13 +14,40 @@ class Item():
         return self._price
 
 
+class CheckoutItem():
+    def __init__(self, item, count=0):
+        self._item = item
+        self._count = count
+
+    @property
+    def count(self):
+        return self._count
+
+    @count.setter
+    def count(self, val):
+        self._count = val
+
+    @property
+    def item(self):
+        return self._item
+
+
 class CheckoutRegister():
     def __init__(self):
-        self._products = []
+        self._products = {}
 
     def scan(self, item):
-        self._products.append(item.price)
+        prod = self._products.get(item.sku, CheckoutItem(item))
+        prod.count += 1
+        self._products[item.sku] = prod
+
+    @property
+    def unique_item_count(self):
+        return len(self._products.keys())
 
     @property
     def total(self):
-        return sum(self._products)
+        return sum([p.count * p.item.price for sku, p in self._products.items()])
+
+
+__all__ = ['Item', 'CheckoutRegister']
--- supermarket/tests.py
+++ supermarket/tests.py
@@ -6,18 +6,25 @@ class SupermarketCheckoutTestCase(unittest.TestCase):
     def setUp(self):
         self.target = CheckoutRegister()
 
+    @staticmethod
+    def _get_items(count):
+        return [Item('i{0}'.format(i), i * 10) for i in range(1, count+1)]
+
+    def _scan_items(self, count):
+        for item in self._get_items(count):
+            self.target.scan(item)
+
     def test_when_item_added_total_price_is_updated(self):
-        item1 = Item('i1', 10)
-        self.target.scan(item1)
+        self._scan_items(1)
 
         self.assertEqual(self.target.total, 10)
 
     def test_multiple_item_prices_calculated_correctly(self):
-        item1 = Item('i1', 10)
-        item2 = Item('i2', 30)
+        self._scan_items(2)
 
-        self.target.scan(item1)
-        self.target.scan(item2)
+        self.assertEqual(self.target.total, 30)
 
-        self.assertEqual(self.target.total, 40)
+    def test_checkout_with_multiple_items_shows_correct_item_count(self):
+        self._scan_items(2)
 
+        self.assertEqual(self.target.unique_item_count, 2)
