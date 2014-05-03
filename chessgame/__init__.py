59b6c07a03a3d90728ab08046714b586f313d618
Taylan Aydinli <taylanaydinli@gmail.com>
2014-05-03 15:20:16 +0300

    Initial implementation of ChessBoard and ChessPiece
    Some tests on Piece initialization.

---
+++ chessgame/__init__.py
@@ -0,0 +1,47 @@
+class ChessBoard():
+    _width = 8
+    _height = 8
+
+    @property
+    def width(self):
+        return self._width
+
+    @property
+    def height(self):
+        return self._height
+
+
+class ChessPiece():
+    RANKS = 'abcdefgh'
+    FILES = list(range(1, 9))
+
+    @staticmethod
+    def get_position_from_notation(notation):
+        if len(notation) != 2 or \
+                notation[0].lower() not in ChessPiece.RANKS or \
+                int(notation[1]) not in ChessPiece.FILES:
+            raise InvalidNotationException('Algebraic Notation must be in '
+                                           'format "AN" where A is'
+                                           'the alpha rank, and N is'
+                                           'the numeric file.')
+        return notation[0], int(notation[1])
+
+    def __init__(self, notation=None, rank=None, file=None):
+        if notation:
+            self._rank, self._file = self.get_position_from_notation(notation)
+        else:
+            self._rank, self._file = rank, file
+
+        if self._rank and self._rank not in ChessPiece.RANKS:
+            raise InvalidNotationException('Rank must be one of {0}'.format(
+                ', '.join(ChessPiece.RANKS)))
+        if self._file and self._file not in ChessPiece.FILES:
+            raise InvalidNotationException('Files must be one of {0}'.format(
+                ','.join(ChessPiece.FILES)))
+
+    @property
+    def position(self):
+        return 'a' if self._rank and self._file else None
+
+
+class InvalidNotationException(Exception): pass
---
+++ chessgame/tests.py
@@ -0,0 +1,45 @@
+import unittest
+from chessgame import ChessBoard, ChessPiece, InvalidNotationException
+
+
+class ChessBoardTestCase(unittest.TestCase):
+    def test_chess_board_size_is_8_by_8(self):
+        cb = ChessBoard()
+        self.assertEqual(8, cb.width)
+        self.assertEqual(8, cb.height)
+
+
+class ChessPieceTestCase(unittest.TestCase):
+    def test_new_chess_piece_has_no_position(self):
+        cp = ChessPiece()
+        self.assertIsNone(cp.position)
+
+    def test_chess_piece_can_be_initialized_with_rank_and_file(self):
+        cp = ChessPiece(rank='a', file=1)
+        self.assertIsNotNone(cp.position)
+
+    def test_init_fails_with_invalid_rank(self):
+        with self.assertRaises(InvalidNotationException):
+            ChessPiece(rank='x', file=2)
+
+    def test_chess_piece_can_be_initialized_with_algebraic_notation(self):
+        cp = ChessPiece(notation='a1')
+        self.assertIsNotNone(cp.position)
+
+    def test_init_with_algebraic_notation_fails_with_invalid_rank(self):
+        with self.assertRaises(InvalidNotationException):
+            ChessPiece(notation='x1')
+
+    def test_init_with_algebraic_notation_fails_with_invalid_file(self):
+        with self.assertRaises(InvalidNotationException):
+            ChessPiece(notation='a9')
+
+    def test_init_with_algebraic_notation_fails_with_malformed_notation(self):
+        with self.assertRaises(InvalidNotationException):
+            ChessPiece(notation='A9l')
+
+        with self.assertRaises(InvalidNotationException):
+            ChessPiece(notation='a')
+
+        with self.assertRaises(InvalidNotationException):
+            ChessPiece(notation='8')
